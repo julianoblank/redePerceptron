9541f90654f3f8ee2b1a9d7f48c32c8a3eb57a10
juliano <julianoellwanger@gmail.com>
2020-03-14 19:48:29 -0300

    inicio

---
+++ redePerceptron.py
@@ -0,0 +1,13 @@
+
+var = int(input( 'Digite sua idade: '))
+
+
+
+def entradas(var1):
+    
+    print('sua idade é: ', var1)
+    while(var1 < 40):
+        var1 = int(input( 'Digite sua idade: '))
+        print('sua idade é: ', var1)
+
+entradas(var)
